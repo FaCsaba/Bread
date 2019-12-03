0a6990d6a5e8189002cc127d1cd9ceaf11c2bd1d
FaCsaba <csab.faz.2@gmail.com>
2019-12-03 23:08:40 +0100

    added leaderboard

--- index.js
+++ index.js
@@ -4,13 +4,56 @@ require('@exan/envreader').load()
 
 let breadBot = new Discord.Client();
 
-const scoreboard = new Discord.RichEmbed().setTitle('The breadest of them all!');
+
+const reactiontimeinminutes = process.env.timebeforereactioncount * 1000 * 60;
+amountOfPeopleOnTheLeaderboard = process.env.amountonleaderboard
+
+function createLeaderBoard(json, message) {
+	leaderboard = new Discord.RichEmbed().setTitle('The breadest of them all!');
+	var sortedArray = [];
+	for (var i in json) {
+		sortedArray.push([i, json[i]]);
+	}
+	sortedArray = sortedArray.sort(function (a, b) { return b[1] - a[1] });
+
+	if (amountOfPeopleOnTheLeaderboard > Object.keys(json).length) {
+		amountOfPeopleOnTheLeaderboard = Object.keys(json).length
+	}
+	for (i = 0; i < amountOfPeopleOnTheLeaderboard; i++) {
+		userId = sortedArray[i][0]
+		breadPoints = sortedArray[i][1]
+
+		message.guild.fetchMember(userId.toString()).then(guildMember => {
+			if (i = 1) {
+				emoji = '🥇'
+			} else if (i = 2) {
+				emoji = '🥈'
+			} else if (i = 3) {
+				emoji = '🥉'
+			} else {
+				emoji = ''
+			}
+			name = guildMember.displayName
+
+			leaderboard.addField(emoji, name, true)
+			leaderboard.addField('With:', breadPoints + ' amount of bread', true)
+			message.channel.send(leaderboard)
+		})
+
+	}
+
+};
+
+
 
 breadBot.on('error', (e) => { });
 
 breadBot.on('message', (message) => {
-	const filter = (reaction, user) => {
-		return reaction.emoji.name === '🍞' && user.id === message.author.id;
+	filter = (reaction, user) => {
+		if (user.id != message.author.id) {
+			return
+		}
+		return (reaction.emoji.name === '🍞' || reaction.emoji.name === '🥖' && (user.id != message.author.id)) //needs test with multiple people
 	};
 
 	if (fs.existsSync('./data/' + message.channel.id + '.json') == false) {
@@ -24,11 +67,10 @@ breadBot.on('message', (message) => {
 		message.channel.name.toLowerCase().includes('🍞')
 	) {
 		if (process.env.messagecollectionbydefault == 'true') {
-			collector = message.createReactionCollector(filter, { time: 1500 });
 
-			collector.on('end', collected => {
-				for (var [key, value] of collected) {
-					for (var [key2, value2] of value.users)
+			message.awaitReactions(filter, { Max: 999999, time: reactiontimeinminutes }).then(reactions => {
+				for (var [key, value] of reactions) {
+					for (var [key2, value2] of value.users) {
 						if (value2.bot == false) {
 							UserId = value2.id
 							var json = JSON.parse(fs.readFileSync('./data/' + message.channel.id + '.json'));
@@ -36,33 +78,43 @@ breadBot.on('message', (message) => {
 
 							if (json[UserId] === undefined) {
 								json[UserId] = 1;
-								console.log('oofs')
 								userData = JSON.stringify(json)
 								fs.writeFileSync('./data/' + message.channel.id + '.json', userData)
 							}
 							else {
 								json[UserId] = json[UserId] + 1
-								console.log(json)
 								userData = JSON.stringify(json)
 								fs.writeFileSync('./data/' + message.channel.id + '.json', userData)
 							}
 						}
-
+					}
 				}
 			})
+
+
+
+
+
 		}
 		if (message.author.id !== breadBot.user.id)
-			if (message.content.startsWith('🍞board')) {
+			if (message.content.startsWith('🍞board') || message.content.startsWith('breadboard')) {
 				if (message.content.includes('-start')) {
 					messagecollection = true
 					message.channel.send("started collection")
 				}
+				if (message.content.includes('-stop')) {
+					messagecollection = true
+					message.channel.send("collection stopped")
+				}
+				if (message.content.includes('-h')) {
+					messagecollection = true
+					message.channel.send("**b r e a d** \nPrints the amount of bread you have collected")
+				}
 				else {
 					var json = JSON.parse(fs.readFileSync('./data/' + message.channel.id + '.json'))
-					for (i = 1; i < process.env.amountonleaderboard; i++) {
 
-					}
-					message.channel.send()
+					createLeaderBoard(json, message)
+
 				}
 			}
 			else if (
@@ -71,7 +123,7 @@ breadBot.on('message', (message) => {
 			) {
 				message.react('🥖');
 			} else if (message.author.id !== breadBot.user.id) {
-				message.react('🍞');
+				message.react('🥖');
 			}
 	}
 });
